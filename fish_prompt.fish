fd520274804573bb8831415b2cc8e30c27b13f64
Roberto Manzella <zelphir@gmail.com>
2019-10-22 14:05:21 +0100

    Improve perf

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,29 +3,42 @@ set -g __pureish_new_session 1
 function fish_prompt
   set -g last_status $status
 
-  set -l left_prompt (__pureish_left_prompt)
-  set -l right_prompt (__pureish_right_prompt)
-  set -q left_prompt;
-    and set -l left_prompt_width (__pureish_string_width $left_prompt);
-    or set -l left_prompt_width 0
-  set -q right_prompt;
-    and set -l right_prompt_width (__pureish_string_width $right_prompt);
-    or set -l right_prompt_width 0
-  set -l spaces (math $COLUMNS - $left_prompt_width - $right_prompt_width)
+  function render_prompt
+    set -l left_prompt (__pureish_left_prompt)
+    set -l right_prompt (__pureish_right_prompt)
+    set -q left_prompt;
+      and set -l left_prompt_width (__pureish_string_width $left_prompt);
+      or set -l left_prompt_width 0
+    set -q right_prompt;
+      and set -l right_prompt_width (__pureish_string_width $right_prompt);
+      or set -l right_prompt_width 0
+    set -l spaces (math $COLUMNS - $left_prompt_width - $right_prompt_width)
 
-  if test $left_prompt_width -lt (math $COLUMNS - 5)
-    if test $__pureish_new_session -eq 0
+    if test $left_prompt_width -lt (math $COLUMNS - 5)
+      if test $__pureish_new_session -eq 0 # add new line
+        echo
+      end
+
+      echo -n $left_prompt
+    end
+
+    printf "%-"$spaces"s" " " # render spaces
+
+    if test (math "$COLUMNS - $left_prompt_width") -gt (math $right_prompt_width + 10)
+      echo $right_prompt
+    else
       echo
     end
-    echo -n $left_prompt
   end
 
-  printf "%-"$spaces"s" " " # render spaces
+  function hide_show_prompt --on-event fish_postexec
+    if test -n $argv[1]
+      render_prompt
+    end
+  end
 
-  if test (math "$COLUMNS - $left_prompt_width") -gt (math $right_prompt_width + 10)
-    echo $right_prompt
-  else
-    echo
+  if test $__pureish_new_session -eq 1
+    render_prompt
   end
 
   echo -n (__pureish_symbol)
--- functions/__pureish_nodejs.fish
+++ functions/__pureish_nodejs.fish
@@ -1,9 +1,8 @@
 function __pureish_nodejs
   if type -q node
-    set -l node_version (string trim -l -c=v (node -v 2>/dev/null))
-    set -l icon_nodejs "$pureish_color_02"
-
     if test -f "package.json"
+      set -l icon_nodejs "$pureish_color_02"
+      set -l node_version (string trim -l -c=v (command asdf current nodejs | awk '{print $1}' 2>/dev/null))
       echo "$icon_nodejs $pureish_color_246$node_version"
     end
   end
